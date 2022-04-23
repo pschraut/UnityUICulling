aa1ce6486451461784ef5df8dbcfc970e7d23e5c
pschraut <p-github@console-dev.de>
2022-04-23 11:12:49 +0200

    added sample

---
+++ Samples~/Sample 1/Scripts/UICullingSample1.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UICullingSample1 : MonoBehaviour
+{
+    [SerializeField] UnityEngine.UI.Text m_Text;
+
+    void OnBecameVisible()
+    {
+        m_Text.text = $"{name}\nInside Viewport";
+        Debug.Log($"{name}: OnBecameVisible", this);
+    }
+
+    void OnBecameInvisible()
+    {
+        m_Text.text = $"{name}\nOutside Viewport";
+        Debug.Log($"{name}: OnBecameInvisible", this);
+    }
+}
